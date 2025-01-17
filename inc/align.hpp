e293dca6abd51c632e78dd394da51d44a3b48f62
Håvard Nygård <haavardn@uia.no>
2025-01-17 19:18:46 +0100

    Moved to a more sensible plan of exposing the HAL, but with  a OK configuration for the STM32G473

--- inc/align.hpp
+++ inc/align.hpp
@@ -1,7 +1,6 @@
 #ifndef ALIGN_HPP
 #define ALIGN_HPP
 
-#include <align_config.hpp>
 #include <cstdint>
 
 #define STM32G431xx   // Temporary to test the code
@@ -10,36 +9,13 @@ namespace LL {
     #include <stm32g4xx_ll_gpio.h>
 }
 
-
-#define ALIGN_BEGIN namespace Align {
-#define ALIGN_END }
-
-
-ALIGN_BEGIN // Start of Align Namespace
-
-typedef enum {
-    A,
-    B,
-    C,
-    D,
-    E
-} GPIO;
-
-class Pin {
-    
-    public:
-    Pin(GPIO gpio, uint8_t pinIdx){
-        GPIO_InitStruct.Pin = 1 << pinIdx;
-        GPIO_InitStruct.Mode = mode;
-        GPIO_InitStruct.OutputType = output;
-        GPIO_InitStruct.Pull = pupd;
-        GPIO_InitStruct.Speed = LL::LL_GPIO_SPEED_FREQ_MEDIUM;
-        LL::LL_GPIO_Init((GPIO_TypeDef*)gpio, &GPIO_InitStruct);
-        
-    }
-
+namespace HAL {
+    #include <stm32g4xx_hal.h>
 }
 
 
-ALIGN_END
+namespace Align {
+    void init();
+}
+
 #endif ALIGN_HPP
